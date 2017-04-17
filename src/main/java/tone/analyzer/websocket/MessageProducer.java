29832baaece5a1a0542c4011182b343d4c3c4408
mozammal <mozammal@nascenia.com>
2017-04-17 16:53:02 +0600

    add  spring security

--- src/main/java/tone/analyzer/websocket/MessageProducer.java
+++ src/main/java/tone/analyzer/websocket/MessageProducer.java
@@ -22,7 +22,7 @@ public class MessageProducer {
 
   public void sendMessageToRecipient(ChatMessage chatMessage) {
 
-    this.template.convertAndSend("/topic/message" + "-" + chatMessage.getRecipient(), chatMessage);
+    this.template.convertAndSend("/topic/chat.message" + "-" + chatMessage.getRecipient(), chatMessage);
   }
 
   public void sendMessageForLiveUser(LoginEvent loginevent) {
