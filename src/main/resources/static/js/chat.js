05a0418cd030f468baad3c779154dfa94f1fec19
mozammal <mozammal@nascenia.com>
2017-05-02 16:51:30 +0600

    js included in local folder

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
     var chatList = $(".chat_body");
     var chatBox = $("#chatbox-container");
     var host = location.protocol + '//' + location.host;
+    console.log("host: "+host);
     var socket = new SockJS(host + "/stomp");
     var stompClient = Stomp.over(socket);
 
