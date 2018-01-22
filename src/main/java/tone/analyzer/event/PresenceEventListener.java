59f0ca5f7016177bacc6b64a748c759ae0a73ac2
mozammal hossain <mozammaltomal.1001@gmail.com>
2018-01-23 00:42:24 +0600

    add scalabilty

--- src/main/java/tone/analyzer/event/PresenceEventListener.java
+++ src/main/java/tone/analyzer/event/PresenceEventListener.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
@@ -31,92 +33,95 @@ import tone.analyzer.domain.repository.ParticipantRepository;
 @Component
 public class PresenceEventListener {
 
-  private static final Logger log = LoggerFactory.getLogger(PresenceEventListener.class);
+    private static final Logger log = LoggerFactory.getLogger(PresenceEventListener.class);
 
-  private ParticipantRepository participantRepository;
+    private ParticipantRepository participantRepository;
 
-  private SimpMessagingTemplate messagingTemplate;
+    private SimpMessagingTemplate messagingTemplate;
 
-  private String loginDestination;
+    private String loginDestination;
 
-  private String logoutDestination;
+    private String logoutDestination;
 
-  @Value("${app.user.login}")
-  private String loginTopic;
+    @Value("${app.user.login}")
+    private String loginTopic;
 
-  private Map<String, String> sessionToUserIdMap;
+    private Map<String, String> sessionToUserIdMap;
 
-  @Autowired private AccountRepository accountRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
-  @Autowired private UserAccountDao userAccountDao;
+    @Autowired
+    private UserAccountDao userAccountDao;
 
-  public PresenceEventListener(
-      SimpMessagingTemplate messagingTemplate, ParticipantRepository participantRepository) {
-    this.messagingTemplate = messagingTemplate;
-    this.participantRepository = participantRepository;
-    this.sessionToUserIdMap = new ConcurrentHashMap<>();
-  }
+    @Autowired
+    SimpUserRegistry simpUserRegistry;
 
-  public ParticipantRepository getParticipantRepository() {
+    public PresenceEventListener(
+            SimpMessagingTemplate messagingTemplate, ParticipantRepository participantRepository) {
+        this.messagingTemplate = messagingTemplate;
+        this.participantRepository = participantRepository;
+        this.sessionToUserIdMap = new ConcurrentHashMap<>();
+    }
 
-    return participantRepository;
-  }
+    public ParticipantRepository getParticipantRepository() {
 
-  @EventListener
-  private void handleSessionConnected(SessionConnectEvent event) {
-    SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
-    String userName = headers.getUser().getName();
+        return participantRepository;
+    }
 
-    Account account = accountRepository.findByName(userName);
+    @EventListener
+    private void handleSessionConnected(SessionConnectEvent event) {
+        SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        String userName = headers.getUser().getName();
 
-    LoginEvent loginEvent = new LoginEvent(userName);
-    loginEvent.setId(account.getId());
-    /*if (participantRepository.getParticipant(userName) == null) {*/
-      List<LoginEvent> onlineBuddyList = userAccountDao.retrieveBuddyList(userName);
-      for (LoginEvent loginEvent1 : onlineBuddyList) {
-        messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
-                loginTopic, loginEvent);
+        Account account = accountRepository.findByName(userName);
 
-        messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
-                "/topic/registry.broadcast", loginEvent);
+        LoginEvent loginEvent = new LoginEvent(userName);
+        loginEvent.setId(account.getId());
 
+        for (SimpUser currentUser : simpUserRegistry.getUsers()) {
+        }
+        List<LoginEvent> onlineBuddyList = userAccountDao.retrieveBuddyList(userName);
+        for (LoginEvent loginEvent1 : onlineBuddyList) {
+            messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
+                    loginTopic, loginEvent);
 
+            messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
+                    "/topic/registry.broadcast", loginEvent);
+        }
+        String sessionId = headers.getSessionId();
+        log.info("logged in user session id: {}", sessionId);
+        sessionToUserIdMap.put(sessionId, userName);
+        // We store the session as we need to be idempotent in the disconnect event processing
+        participantRepository.add(userName, loginEvent);
+    }
 
-      }
-   /* }*/
-    String sessionId = headers.getSessionId();
-    log.info("logged in user session id: {}", sessionId);
-    sessionToUserIdMap.put(sessionId, userName);
-    // We store the session as we need to be idempotent in the disconnect event processing
-    participantRepository.add(userName, loginEvent);
-  }
+    @EventListener
+    private void handleSessionDisconnect(SessionDisconnectEvent event) {
+        log.info("logged out user session id: {}", event.getSessionId());
+        Optional.ofNullable(
+                participantRepository.getParticipant(sessionToUserIdMap.get(event.getSessionId())))
+                .ifPresent(
+                        login -> {
+                            List<LoginEvent> onlineBuddyList =
+                                    userAccountDao.retrieveBuddyList(login.getUserName());
+                            for (LoginEvent loginEvent1 : onlineBuddyList) {
+                                messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
+                                        logoutDestination,
+                                        new LogoutEvent(login.getUserName(), login.getId()));
 
-  @EventListener
-  private void handleSessionDisconnect(SessionDisconnectEvent event) {
-    log.info("logged out user session id: {}", event.getSessionId());
-    Optional.ofNullable(
-            participantRepository.getParticipant(sessionToUserIdMap.get(event.getSessionId())))
-        .ifPresent(
-            login -> {
-              List<LoginEvent> onlineBuddyList =
-                  userAccountDao.retrieveBuddyList(login.getUserName());
-              for (LoginEvent loginEvent1 : onlineBuddyList) {
-                messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
-                        logoutDestination,
-                        new LogoutEvent(login.getUserName(), login.getId()));
+                                messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
+                                        "/topic/registry.broadcast", new LogoutEvent(login.getUserName(), login.getId()));
+                            }
+                            participantRepository.removeParticipant(sessionToUserIdMap.get(event.getSessionId()));
+                        });
+    }
 
-                messagingTemplate.convertAndSendToUser(loginEvent1.getUserName(),
-                        "/topic/registry.broadcast", new LogoutEvent(login.getUserName(), login.getId()));
-              }
-              participantRepository.removeParticipant(sessionToUserIdMap.get(event.getSessionId()));
-            });
-  }
+    public void setLoginDestination(String loginDestination) {
+        this.loginDestination = loginDestination;
+    }
 
-  public void setLoginDestination(String loginDestination) {
-    this.loginDestination = loginDestination;
-  }
-
-  public void setLogoutDestination(String logoutDestination) {
-    this.logoutDestination = logoutDestination;
-  }
+    public void setLogoutDestination(String logoutDestination) {
+        this.logoutDestination = logoutDestination;
+    }
 }
