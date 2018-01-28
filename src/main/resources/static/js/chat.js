bbe173ad731b0b1699347b5fe332150e16a8bc28
mozammal hossain <mozammaltomal.1001@gmail.com>
2018-01-28 23:47:16 +0600

    removed unnecssary js for facebook sharing

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -265,7 +265,7 @@ $(document).ready(function () {
         }
       });
     });
-    
+
     $("#myModal").on("show.bs.modal", function (e) {
       var link = $(e.relatedTarget);
       $(this).find(".modal-body").load(link.attr("href"));
