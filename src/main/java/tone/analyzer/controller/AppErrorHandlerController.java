bfa085bf86b50c69c3105469d9c0f6ae564c5658
mozammal <mozammal@nascenia.com>
2018-04-18 17:33:36 +0600

    changed app properties

--- src/main/java/tone/analyzer/controller/AppErrorHandlerController.java
+++ src/main/java/tone/analyzer/controller/AppErrorHandlerController.java
@@ -3,10 +3,6 @@ package tone.analyzer.controller;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-
 
 @Controller
 public class AppErrorHandlerController implements ErrorController {
